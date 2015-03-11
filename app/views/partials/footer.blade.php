da5ad17bb0edb1b04db07531bb70e079a5d82b74
Jamie Finn <jamiefinn.jf@gmail.com>
2015-03-11 09:36:48 +0000

    Added date function on line 6 to automatically output the current year

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
       <footer>
         <div class="row">
           <div class="col-lg-12">
-            <p>Copyright &copy; Jay's Blog 2014 &middot; {{ HTML::link('http://www.facebook.com/jamie.finn.963', '', array('class' => 'fa fa-facebook-square', 'id' => 'footerLink')) }} &middot; {{ HTML::link('http://twitter.com/JamieF2591', '', array('class' => 'fa fa-twitter', 'id' => 'footerLink')) }} </p>
+            <p>Copyright &copy; Jay's Blog {{ date('Y') }} &middot; {{ HTML::link('http://www.facebook.com/jamie.finn.963', '', array('class' => 'fa fa-facebook-square', 'id' => 'footerLink')) }} &middot; {{ HTML::link('http://twitter.com/Jays_Blog', '', array('class' => 'fa fa-twitter', 'id' => 'footerLink')) }} </p>
           </div>
         </div>
       </footer>
