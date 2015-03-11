63550dbb2411b34201041ca0053a66a99c15b9f4
Jamie Finn <jamiefinn.jf@gmail.com>
2015-03-11 10:13:30 +0000

    Added date function in footer on line 57 to output the current year

--- app/views/layouts/admin.blade.php
+++ app/views/layouts/admin.blade.php
@@ -54,7 +54,7 @@
       <footer>
         <div class="row">
           <div class="col-lg-12">
-            <p>Copyright &copy; Jay's Blog 2014 &middot; Facebook &middot; Twitter &middot; Google+</p>
+            <p>Copyright &copy; Jay's Blog {{ date('Y') }}.</p>
           </div>
         </div>
       </footer>
